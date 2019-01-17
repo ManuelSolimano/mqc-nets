b7c7d9845581e81d26b80887cf9642ad8f9dcb7b
Manuel Solimano <masolimano@uc.cl>
2019-01-17 19:39:37 -0300

    Implemented specobjid hashing and file size reduction routine

--- pipeline.py
+++ pipeline.py
@@ -31,18 +31,94 @@ def normalize(spectrum):
     normspec['flux'] = normflux
     return normspec
 
-def tell_the_truth(plate, mjd, fiberid):
+def hash_specid(plate, mjd, fiberid, run2d):
     """
-    Database lookup to determine classification
-    params:
-        -plate: plate id
-        -mjd: modified julian date
-        -fiberid: fiber id
-    returns:
-        -Y: truth vector indicating class
+    Hash function that computes the SpecObj ID from the four spectral
+    identifiers. This implementation follows the specification found at
+    https://www.sdss.org/dr15/help/glossary/#specobj   
+    This function can return very long integers, so make sure
+    to store them as unsigned 64 bit integers.
+    
+    parameters:
+     -plate: int, Drilled plate number
+     -mjd :int, Modified Julian Date (days)
+     -fiberid: int, Fiber ID number
+     -run2d: int or str, Version of 2D reduction pipeline
+     
+     returns:
+         -specid: int, SpecObjID
     """
-    pass
+    
+    run2d = str(run2d)
+    
+    if 'v' in run2d:
+        # The string must have shape 'vN_M_P' where N, M and P are integers
+        nmp = run2d.split('_')
+        N = int(nmp[0].strip('v'))
+        M = int(nmp[1])
+        P = int(nmp[2])
+        assert 5 <= N <= 6 and 0 <= M <= 99 and 0 <= P <=99, "Invalid run2d"
+        run2d  = (N - 5) * 10000 + M * 100 + P
+        
+    elif run2d.isnumeric():
+        run2d = int(run2d)
 
+    key = '{:014b}'.format(plate)    
+    key += '{:012b}'.format(fiberid)
+    key += '{:014b}'.format(mjd - 50000)
+    key += '{:014b}'.format(run2d)
+    key += '0' * 10
+    
+    specid = int(key, base=2)
+    return specid
+
+def tell_the_truth(filename):
+    """ Return the truth vector for a given spectrum filename.
+    Constants KNOWN_QSOS and KNOWN_STARS must be loaded in memory.
+    parameters:
+        -filename: str, the path to the spectrum fits file
+    return:
+        -truth: array, [qso_prob, redshift]
+    
+    TODO: Implement redshift  retrieval from fits file or specobjid
+    """
+    header = fitsio.read_header(filename)
+    spec4numbers = (header['PLATEID'], header['MJD'], header['FIBERID'], header['RUN2D'].strip())
+    specid = hash_specid(*spec4numbers)  # Compute specobjid hash 
+    
+    if specid in KNOWN_QSOS:
+        return np.array([1., 0.])
+    
+    elif specid in KNOWN_STARS:
+        return np.array([0., 0.])
+    
+    else:
+        return None
+    
+def shrink_spectra(filepath):
+    """
+    Remove unnecesary data from the spectral FITS file. All
+    the data needed is contained in the header and the
+    flux, loglam and ivar columns. This function
+    trims the original fits file and saves the lightweight
+    function in a {parent}_ultralight folder. Then deletes the original
+    file.
+    """
+    pathlist = filepath.split('/')
+    newpath = '{}_ultralight/{}'.format('/'.join(pathlist[:-1]), pathlist[-1])
+    
+    temp = tempfile.NamedTemporaryFile(delete=False)
+    with fits.open(filepath) as hdul:
+        relevant_cols = hdul[1].data.columns[:3]
+        hdul[1].data = hdul[1].data.from_columns(relevant_cols)
+        hdul = hdul[:2]
+        hdul.writeto(temp)
+        
+    os.remove(filepath)
+    print('Removed ' + filepath)
+    shutil.move(temp.name, newpath)
+    print('Created ' + newpath)
+    temp.close()
 
 def crop_and_subsample(nspec, pixels, borders):
     """
