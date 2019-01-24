6c6b95b801c1fefce4a3406c6e66f3c118961ea8
Manuel Solimano <masolimano@uc.cl>
2019-01-24 19:52:25 -0300

    Selected a (more)  homogeneous sample of stars and finished QSO pipelinereduction

--- .ipynb_checkpoints/issues-checkpoint.md
+++ .ipynb_checkpoints/issues-checkpoint.md
@@ -8,6 +8,7 @@
 7. Great, I spend a lot of time collecting the photometry of DR14Q sources while the catalog fits file included the ugriz magnitudes!
 8. I have to downgrade to Python 3.6 because TensorFlow doesn't support Python 3.7, this means creating a new environment
 9. I initially thought the most narrow limits for ``loglam`` were ``[3.56-4.01]``, however, in the starboss files I found spectra with limits as narrow as ``[3.5579-3.8018]``. This is a problem because interpolation cannot be done outside these limits, and if I just crop the spectra I may end with smaller and smaller spectra. 
+10. I expected that using a bigger dataset (35K spectra versus 2K), would immediately yield better results with the same architecture. After fiddling with the model and its parameters in Collab I was pretty disappointed because I cannot get the model to learn anything. It is stuck on loss ~0.6. Also the binary accuracy on the validation set is 0.4969 at the end of almost every epoch. Need to 1) tune those hyperparameters and/or 2) keep trying new  architectures (deeper or shallower) 
 TODO:
 - [x] Find a way to read the latest version of the MQC
 - [X] Keep only the spectroscopically confirmed quasars
@@ -15,6 +16,8 @@ TODO:
 - [X] Prototype spectrum reduction pipeline (subsampling and interpolation)
 - [X] Fix interpolation outside range issue
 - [X] Add class information to header of spectra, or better, create a lookup table
+- [ ] Study what ``scipy.signal.resample`` is doing behind the screens
+- [ ] Get the new model working on the extended dataset (may be necessary to check the file is OK)
 - [ ] Include line recognition flags, i.e should the model be a line finder?
 - [ ] (almost done) Make a training/testing set 1 using ugriz photometry from DR15, known classes and redshifts, the set should also include non quasar PLOs
 - [ ] Make a training/testing  set 2 using the spectra of the known quasars and stars, together with its redshift. This dataset will probably be smaller than dataset 1, but could better justify the use of deep-learning because of the automatic feature extraction
--- preprocess.py
+++ preprocess.py
@@ -6,8 +6,16 @@ import h5py
 import os
 
 sampling = 433
-qsoglob = glob('data/quasarboss/*.fits')[:1000]
-starglob = glob('data/starboss/*.fits')[:1000]  # These files aren't reduced in size but may still work
+# qsoglob = glob('data/quasarboss/*.fits')[:1000]
+# starglob = glob('data/starboss/*.fits')[:1000]  # These files aren't reduced in size but may still work
+
+qsoglob = np.array(glob('data/quasarboss/*.fits'))
+qsoglob = qsoglob[np.random.choice(qsoglob.size, 17750, replace=False)].tolist()
+
+with open('data/filelist_starboss_homosample_17750.txt', 'r') as sb:
+    starglob = []
+    for line in sb:
+        starglob.append(line.strip())
 
 fileglob = qsoglob + starglob
 
@@ -16,23 +24,34 @@ number_of_files = len(fileglob)
 X_array = np.zeros((number_of_files, sampling, ))
 Y_array = np.zeros((number_of_files, 1,))
 
-try:
-    for i, filename in enumerate(fileglob):
+count = 0
+for i, filename in enumerate(fileglob):
+    try:
         x, y = io.load_1d_spectrum(filename, sampling=sampling)
         X_array[i,...] = x
         Y_array[i,...] = y
-        if i % 10 == 0:
-            print('Processed  {} files so far'.format(i + 1))
-except ValueError:
-    print(filename + ' caused problems')
+        count += 1
+        
+    except ValueError:
+        # I forgot the actual bug behind this :c
+        print(filename + ' caused problems')
+    
+    except OSError:
+        # Apparently fitsio cannot find some files
+        print(filename + ' Fitsio failed to open')
+    
+    if count % 10 == 0:
+        print('Processed  {} files so far'.format(count))
 
-if os.path.exists('data/dataset2K_433t.h5'):
-    os.remove('data/dataset2K_433t.h5')
 
-with h5py.File('data/dataset2K_433t.h5') as hdf:       
+outfile = 'data/dataset{}_{}t.h5'.format(count, sampling)
+if os.path.exists(outfile):
+    os.remove(outfile)
+
+with h5py.File(outfile) as hdf:       
     X_dset  = hdf.create_dataset('fluxes', shape=(number_of_files, sampling, ), dtype='f4')
     Y_dset = hdf.create_dataset('classes', shape=(number_of_files, 1,), dtype='f4')
     X_dset[:] = X_array
     Y_dset[:] = Y_array
 
-print('Done with {} files'.format(i + 1))
+print('Done with {} files'.format(count))
